ae3bf81e88b0b698e8883c677c2d19c561a7e78f
iamsatyapati <iamsatyapati@gmail.com>
2024-05-16 08:04:07 +0530

    data fetching done using axios and useEffect

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import React, { useContext, useReducer } from "react";
-import ComponentC from "./components/useReducerDemo/ComponentC";
-import ComponentA from "./components/useReducerDemo/ComponentA";
-import ComponentB from "./components/useReducerDemo/ComponentB";
-
-const initialState = 0;
-
-export const CountContext = React.createContext()
-
-const reducer = (state, action) => {
-    switch(action) {
-        case 'increment':
-            return state+1
-        case 'decrement':
-            return state - 1
-        case 'reset':
-            return initialState
-        default:
-            return state
-    }
-}
-
+import DataFetching1 from "./components/useReducerDemo/DataFetching1";
 
 function App() {
-
-  const [ count, dispatch ] = useReducer(reducer, initialState)
   
   return (
-    <CountContext.Provider value={{ countState: count, countDispath: dispatch }}>
       <div>
-        Count - {count}
-        <ComponentA />
-        <ComponentB />
-        <ComponentC />
+        <DataFetching1 />
       </div>
-    </CountContext.Provider>
   );
 }
 
